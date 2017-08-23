23e75cc7ad753159567d80c1825bd0c89ec69b92
Francis Breidenbach <fbreidenbach@gmail.com>
2017-08-22 23:14:28 -0700

    Basic UI work to create commit. Storyboard and file organization only.

---
+++ Loggit/ViewController/ProjectListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectListViewController.swift
+//  Loggit
+//
+//  Created by Francis Breidenbach on 8/22/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import UIKit
+
+class ProjectListViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+  }
+  
+
+
+}
+
