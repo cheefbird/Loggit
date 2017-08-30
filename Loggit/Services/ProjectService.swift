6e0acde04310bdd89bf0a0afe646a942f88fce0f
Francis Breidenbach <fbreidenbach@gmail.com>
2017-08-30 01:43:03 -0700

    Begin project service.

---
+++ Loggit/Services/ProjectService.swift
@@ -0,0 +1,50 @@
+//
+//  ProjectService.swift
+//  Loggit
+//
+//  Created by Francis Breidenbach on 8/30/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import RxAlamofire
+import SwiftyJSON
+import RealmSwift
+
+
+class ProjectService {
+  
+  func getAllProjects() -> Observable<[Project]> {
+    
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
